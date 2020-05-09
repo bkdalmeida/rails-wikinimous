ea84779ae73a4ff02638511f5981b3a209045cb6
Bernadette d'Almeida <b.dalmeida@hotmail.fr>
2020-05-09 15:18:05 +0200

    links added no CSS

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,10 +20,7 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
-    @article.destroy
-    redirect_to articles_path
   end
-
 
   def update
     @article = Article.find(params[:id])
@@ -33,6 +30,8 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article = Article.find(params[:id])
+   @article.destroy
+    redirect_to articles_path
 
   end
 #----------strong params----------------
